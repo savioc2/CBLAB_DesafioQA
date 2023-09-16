25649207b0d861e31fd2c26abfa9d79ed4e19321
savioc2 <saviocunha61@gmail.com>
2023-09-15 22:29:44 -0300

    adicionado script de teste

--- desafio_test.py
+++ desafio_test.py
@@ -4,11 +4,10 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import ipdb 
+import ipdb
 
 
-
-def test_login_usuario_sem_cadastro():
+def test_login_usuario_cadastrado():
     SITE_LINK = "https://app.cocobambu.com/entrar?backPage=on-boarding%2Fdelivery%3FnextPage%3D%252Fdelivery"
 
     options = webdriver.ChromeOptions()
@@ -30,7 +29,7 @@ def test_login_usuario_sem_cadastro():
         By.XPATH,
         "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/form/div[3]/ion-input/input",
     )
-    senha_input.send_keys("fraca")
+    senha_input.send_keys("Cococ2.")
 
     # Passo 3: Clicar no botão "Entrar"
     entrar_button = driver.find_element(
@@ -39,7 +38,73 @@ def test_login_usuario_sem_cadastro():
     )
     entrar_button.click()
 
-    time.sleep(2)
+    time.sleep(5)
+
+def test_login_usuario_cadastrado_invalido():
+    SITE_LINK = "https://app.cocobambu.com/entrar?backPage=on-boarding%2Fdelivery%3FnextPage%3D%252Fdelivery"
+
+    options = webdriver.ChromeOptions()
+    options.binary_location = "bin/chrome"
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
+    driver.get(SITE_LINK)
+
+    # Passo 1: Informar um email inválido
+    email_input = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/form/div[1]/ion-input/input",
+    )
+    email_input.send_keys("savioc.@gmail.com")
+
+    # Passo 2: Informar uma senha inválida
+    senha_input = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/form/div[3]/ion-input/input",
+    )
+    senha_input.send_keys("Cococ2.")
+
+    # Passo 3: Clicar no botão "Entrar"
+    entrar_button = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/div[2]/button",
+    )
+    entrar_button.click()
+
+    time.sleep(5)
+
+def test_cadastro_usuario():
+    SITE_LINK = "https://app.cocobambu.com/entrar?backPage=on-boarding%2Fdelivery%3FnextPage%3D%252Fdelivery"
+
+    options = webdriver.ChromeOptions()
+    options.binary_location = "bin/chrome"
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
+    driver.get(SITE_LINK)
+
+    # # Passo 1: Informar um email válido
+    # email_input = driver.find_element(
+    #     By.XPATH,
+    #     "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/form/div[1]/ion-input/input",
+    # )
+    # email_input.send_keys("saviocb@gmail.com")
+
+    # # Passo 2: Informar uma senha inválida
+    # senha_input = driver.find_element(
+    #     By.XPATH,
+    #     "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/form/div[3]/ion-input/input",
+    # )
+    # senha_input.send_keys("fraca")
+
+    # # Passo 3: Clicar no botão "Entrar"
+    # entrar_button = driver.find_element(
+    #     By.XPATH,
+    #     "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/div[2]/button",
+    # )
+    # entrar_button.click()
+
+    # time.sleep(2)
 
     # Passo 4: Clicar em "Cadastre-se"
     cadastra_button = driver.find_element(
@@ -81,11 +146,12 @@ def test_login_usuario_sem_cadastro():
     estado_button = wait.until(
         EC.visibility_of_element_located(
             (
-                By.XPATH, "//ion-sel-1-lbl",
+                By.XPATH,
+                "//ion-sel-1-lbl",
                 # "/html/body/app-root/ion-app/div/div/desktop-modal/div[2]/register-popup/div/form/div[9]/ion-select//label",
             )
         )
-    )     
+    )
     estado_button.click()
     ipdb.set_trace()
 
@@ -125,21 +191,115 @@ def test_login_usuario_sem_cadastro():
     )
     termos_button.click()
 
-    # Resultados Esperados
-    time.sleep(30)  # Aguardar para verificação (tempo de espera pode variar)
-    # assert "Tela Principal" in driver.title  # Verifica se o usuário foi redirecionado para a tela principal
-
-    driver.quit()
+    time.sleep(30)
 
 
-# def test_reset_senha():
-#     SITE_LINK = "https://app.cocobambu.com/entrar?backPage=on-boarding%2Fdelivery%3FnextPage%3D%252Fdelivery"
+def test_reset_senha():
+    SITE_LINK = "https://app.cocobambu.com/entrar?backPage=on-boarding%2Fdelivery%3FnextPage%3D%252Fdelivery"
 
-#     options = webdriver.ChromeOptions()
-#     options.binary_location = 'bin/chrome'
-#     driver = webdriver.Chrome(options=options)
-#     driver.maximize_window()
+    options = webdriver.ChromeOptions()
+    options.binary_location = "bin/chrome"
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
 
-#     driver.get(SITE_LINK)
+    driver.get(SITE_LINK)
 
-test_login_usuario_sem_cadastro()
+    # Passo 1: Clicar em Resetar senha
+    reset_senha = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/div[3]/ion-col[2]/span[2]",
+    )
+    reset_senha.click()
+
+    time.sleep(5)
+
+    # Passo 2: Infomar email válido
+    email_input = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/ion-alert/div[2]/div[3]/div/input",
+    )
+    email_input.send_keys("savioc.unb@gmail.com")
+
+    time.sleep(5)
+
+    #wait = WebDriverWait(driver, 5)
+    # Passo 3: Clicar em recuperar
+    # reset_senha = wait.until(
+    #     EC.visibility_of_element_located(
+    #         ("/html/body/app-root/ion-app/ion-alert/div[2]/div[4]/button[2]/span",)
+    #     )
+    # )
+
+    cadastra = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/ion-alert/div[2]/div[4]/button[2]",
+    )
+    cadastra.click()
+    #reset_senha.click()
+    time.sleep(5)
+
+    # ok_button = driver.find_element(
+    #     By.XPATH,
+    #     "/html/body/app-root/ion-app/ion-alert[2]/div[2]/div[3]/button/span",
+    # )
+    # ok_button.click()
+    time.sleep(5)
+
+def test_reset_senha_email_invalido():
+    SITE_LINK = "https://app.cocobambu.com/entrar?backPage=on-boarding%2Fdelivery%3FnextPage%3D%252Fdelivery"
+
+    options = webdriver.ChromeOptions()
+    options.binary_location = "bin/chrome"
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
+    driver.get(SITE_LINK)
+
+    # Passo 1: Clicar em Resetar senha
+    reset_senha = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/main/ion-router-outlet/app-login/div/div/div[3]/ion-col[2]/span[2]",
+    )
+    reset_senha.click()
+
+    time.sleep(5)
+
+    # Passo 2: Infomar email válido
+    email_input = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/ion-alert/div[2]/div[3]/div/input",
+    )
+    email_input.send_keys("errado_email")
+
+    time.sleep(5)
+
+    #wait = WebDriverWait(driver, 5)
+    # Passo 3: Clicar em recuperar
+    # reset_senha = wait.until(
+    #     EC.visibility_of_element_located(
+    #         ("/html/body/app-root/ion-app/ion-alert/div[2]/div[4]/button[2]/span",)
+    #     )
+    # )
+
+    cadastra = driver.find_element(
+        By.XPATH,
+        "/html/body/app-root/ion-app/ion-alert/div[2]/div[4]/button[2]",
+    )
+    cadastra.click()
+    #reset_senha.click()
+    time.sleep(5)
+
+    # ok_button = driver.find_element(
+    #     By.XPATH,
+    #     "/html/body/app-root/ion-app/ion-alert[2]/div[2]/div[3]/button/span",
+    # )
+    # ok_button.click()
+    time.sleep(5)
+
+
+# test_login_usuario_cadastrado()
+test_login_usuario_cadastrado_invalido()
+# test_reset_senha()
+# test_reset_senha_email_invalido()
+# test_cadastro_usuario()
+
